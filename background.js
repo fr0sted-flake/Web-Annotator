230148145b3244b77bb2043a31dc940c6f3078d4
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2024-06-02 14:13:08 +0530

    modified the manifest file

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ annotations: [] });
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ annotations: [] });
+// });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'saveAnnotation') {
-    chrome.storage.sync.get('annotations', (data) => {
-      let annotations = data.annotations || [];
-      annotations.push(request.annotation);
-      chrome.storage.sync.set({ annotations });
-    });
-  }
-});
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === 'saveAnnotation') {
+//     chrome.storage.sync.get('annotations', (data) => {
+//       let annotations = data.annotations || [];
+//       annotations.push(request.annotation);
+//       chrome.storage.sync.set({ annotations });
+//     });
+//   }
+// });
